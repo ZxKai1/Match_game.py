7ba0cc1e8705c108addae6501505c804f30a8aeb
ZxKai1 <kyle.c11124@gmail.com>
2025-12-10 11:59:49 +0800

    Add files via upload

---
+++ Group 3 Matchgame.py/data_structure.py
@@ -0,0 +1,47 @@
+import os
+# data_structure.py
+ICON_PATH = "images/back"
+
+# Gadget image filenames
+SYMBOL_IMAGES = [
+    "camera.png",
+    "cctv.png",
+    "controller.png",
+    "cpu.png",
+    "headphone.png",
+    "keyboard.png",
+    "laptop.png",
+    "mic.png",
+    "mouse.png",
+    "oven.png",
+    "phone.png",
+    "plug.png",
+    "usb.png",
+    "webcam.png",
+    "printer.png",
+    "projector.png",
+    "modem.png",
+    "cd.png"
+]
+
+LEVEL_DATA = {
+    "Easy": {
+        "rows": 3,
+        "cols": 4,
+        "time": 40,
+        
+        
+    },
+    "Medium": {
+        "rows": 4,
+        "cols": 5,
+        "time": 50,
+        
+    },
+    "Hard": {
+        "rows": 4,
+        "cols": 7,
+        "time": 60,
+        
+    }
+}
